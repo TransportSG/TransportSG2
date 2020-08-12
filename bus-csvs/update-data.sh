4c05195282d94661a85326ef33010c3cd11cf313
Eyeball Edward <edwardyeung.private@gmail.com>
2020-08-12 16:38:55 +1000

    new csv links & follow redirect

--- bus-csvs/update-data.sh
+++ bus-csvs/update-data.sh
@@ -1,6 +1,6 @@
 DIRNAME=$(dirname "$0")
 
-curl "https://docs.google.com/spreadsheets/u/1/d/1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs/export?format=csv&id=1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs&gid=819371872" > $DIRNAME/data/SBS.csv
-curl "https://docs.google.com/spreadsheets/u/1/d/1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs/export?format=csv&id=1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs&gid=2137214531" > $DIRNAME/data/SMB.csv
-curl "https://docs.google.com/spreadsheets/u/1/d/1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs/export?format=csv&id=1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs&gid=1503277849" > $DIRNAME/data/SG.csv
-curl "https://docs.google.com/spreadsheets/u/1/d/1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs/export?format=csv&id=1GCRG9OhR8A7pqYUUTm42-RW0VX-9_g0nNH-NB9WdARs&gid=1534016276" > $DIRNAME/data/TIB.csv
+curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Rt5p8Gq6HUWmtNkWkqYl0jkGxrEtCh9li2ibacMwLDJ0GpvinwnRayddrf8zJMXMuYH1zpPH_dPZ/pub?gid=819371872&single=true&output=csv" > $DIRNAME/data/SBS.csv
+curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Rt5p8Gq6HUWmtNkWkqYl0jkGxrEtCh9li2ibacMwLDJ0GpvinwnRayddrf8zJMXMuYH1zpPH_dPZ/pub?gid=2137214531&single=true&output=csv" > $DIRNAME/data/SMB.csv
+curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Rt5p8Gq6HUWmtNkWkqYl0jkGxrEtCh9li2ibacMwLDJ0GpvinwnRayddrf8zJMXMuYH1zpPH_dPZ/pub?gid=1503277849&single=true&output=csv" > $DIRNAME/data/SG.csv
+curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Rt5p8Gq6HUWmtNkWkqYl0jkGxrEtCh9li2ibacMwLDJ0GpvinwnRayddrf8zJMXMuYH1zpPH_dPZ/pub?gid=1534016276&single=true&output=csv" > $DIRNAME/data/TIB.csv
