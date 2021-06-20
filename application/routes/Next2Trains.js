0aa5b955b1e7886442c510bccb9b2c7a03bc0d7d
Eyeball Edward <edwardyeung.private@gmail.com>
2021-06-20 20:13:22 +1000

    split mrt into template

--- application/routes/Next2Trains.js
+++ application/routes/Next2Trains.js
@@ -4,24 +4,33 @@ const router = new express.Router()
 const getDepartures = require('../timings/mrt')
 const async = require('async')
 
-router.get('/:codedStationName', async (req, res) => {
+async function loadDepartures(req, res) {
   let stops = res.db.getCollection('stops')
   let stationData = await stops.findDocument({
     mode: 'mrt',
     codedStopName: req.params.codedStationName
   })
 
-  if (!stationData) return res.end('no departure need to do that screen')
+  if (!stationData) {
+    return res.status(404).render('errors/no-stop')
+  }
 
   let departures = await getDepartures(stationData)
 
-  departures = departures.filter(departure => departure.destination !== 'Do not board')
-
-  res.render('mrt/timings', {
-    departures,
+  return {
+    departures: departures.filter(departure => departure.destination !== 'Do not board'),
     station: stationData,
     timeDifference: utils.prettyTimeToArrival
-  })
+  }
+}
+
+router.get('/:codedStationName', async (req, res) => {
+  let response = await loadDepartures(req, res)
+  if (response) res.render('mrt/timings', response)
+})
+
+router.post('/:codedStationName', async (req, res) => {
+  res.render('mrt/template', await loadDepartures(req, res))
 })
 
 
